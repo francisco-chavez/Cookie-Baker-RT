ff6f25554fc993a8f9c9f605d70790f31640e746
Francisco Chavez-Tejeda <francisco-chavez@users.noreply.github.com>
2019-07-03 17:16:40 -0700

    Added a blank Editor Window for controlling the cookie baking.

---
+++ Unity Projects/Prototype 01/Assets/Cookie Baker RT/Scripts/Editor/CookieBakerEditorWindow.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEditor;
+using UnityEditorInternal;
+using UnityEngine;
+
+
+namespace FCT.CookieBakerP01
+{
+	public class CookieBakerEditorWindow
+		: EditorWindow
+	{
+		[MenuItem("Cookie Baker RT/Editor Window")]
+		public static void Init()
+		{
+			var window = EditorWindow.GetWindow<CookieBakerEditorWindow>("Cookie Baker RT",
+																		 new Type[] { Type.GetType("UnityEditor.InspectorWindow, UnityEditor.dll") });
+		}
+	}
+}
