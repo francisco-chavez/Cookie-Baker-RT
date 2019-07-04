fabf4ae42bfeda21258048453b1a0b87686ef836
Francisco Chavez-Tejeda <francisco-chavez@users.noreply.github.com>
2019-07-03 17:42:04 -0700

    Started work on code for drawing our Editor Window.

--- Unity Projects/Prototype 01/Assets/Cookie Baker RT/Scripts/Editor/CookieBakerEditorWindow.cs
+++ Unity Projects/Prototype 01/Assets/Cookie Baker RT/Scripts/Editor/CookieBakerEditorWindow.cs
@@ -13,11 +13,83 @@ namespace FCT.CookieBakerP01
 	public class CookieBakerEditorWindow
 		: EditorWindow
 	{
+
+		#region Attributes
+
+		/// <summary>
+		/// The current overall state of the bake process.
+		/// </summary>
+		private static BakeState s_currentBakeState = BakeState.SettingSelection;
+
+		#endregion
+
+
+		/// <summary>
+		/// Open the Editor Window.
+		/// </summary>
 		[MenuItem("Cookie Baker RT/Editor Window")]
 		public static void Init()
 		{
 			var window = EditorWindow.GetWindow<CookieBakerEditorWindow>("Cookie Baker RT",
 																		 new Type[] { Type.GetType("UnityEditor.InspectorWindow, UnityEditor.dll") });
 		}
+
+		/// <summary>
+		/// Draw this Window (or the contents of this window).
+		/// </summary>
+		private void OnGUI()
+		{
+			switch (CookieBakerEditorWindow.s_currentBakeState)
+			{
+				case BakeState.SettingSelection:
+					break;
+
+				case BakeState.Prep:
+					break;
+
+				case BakeState.Bake:
+					break;
+
+				case BakeState.Finalize:
+					break;
+
+				default:
+					break;
+			}
+		}
+
+		/// <summary>
+		/// Things that need to be called in a loop and are not automatically called should be here. This is for
+		/// items that only need to be called a few times a second and not as fast a possible. According to the
+		/// docs that I read, this gets called 10 times a second.
+		/// </summary>
+		private void OnInspectorUpdate()
+		{
+			// Normally, OnGUI() is called when a value that we can set in this Window is changed through this 
+			// Window. So, if a value is changed elsewhere, or a piece of data we pull from elsewhere is changed, 
+			// then we don't repaint the window. Placing the repaint call in here will keep the window up to date 
+			// at about 10 fps.
+			// -FCT
+			Repaint();
+		}
+
+
+		#region Internal Struct Definitions
+
+		/// <summary>
+		/// One thing I've learned the hard way about creating tools in Unity is that a bool isn't always enough 
+		/// when you are dealing with multiple threads or different loop stages.
+		/// -FCT
+		/// </summary>
+		private enum BakeState
+		{
+			SettingSelection,
+			Prep,
+			Bake,
+			Finalize
+		}
+
+		#endregion
+
 	}
 }
