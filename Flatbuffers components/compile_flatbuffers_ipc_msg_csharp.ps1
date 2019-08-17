ef5895908612ea61abd82de2754abdeb561fa94f
Francisco Chavez-Tejeda <francisco-chavez@users.noreply.github.com>
2019-08-17 12:29:50 -0700

    I created a PowerShell script that will run flatc.exe and ran it to generate C# code for reading/write data that uses the serialization format I'm using for IPC.

---
+++ Flatbuffers components/compile_flatbuffers_ipc_msg_csharp.ps1
@@ -0,0 +1,2 @@
+﻿
+flatc --csharp --gen-nullable -o "./IPC MSG Serialization Code/CSharp/" "./schema/MyStructs.fbs" "./schema/CancelWorkloadMsg.fbs" "./schema/ProgressUpdateMsg.fbs" "./schema/UpAndRunningMsg.fbs" "./schema/WorkloadCompleteMsg.fbs" "./schema/WorkloadReceivedMsg.fbs" "./schema/WorkloadRequestMsg.fbs" "./schema/MessageRoot.fbs"
