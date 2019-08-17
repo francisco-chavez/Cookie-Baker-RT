07afcaeb96d6662b02e1ef94440d02909b504c07
Francisco Chavez-Tejeda <francisco-chavez@users.noreply.github.com>
2019-08-17 12:45:06 -0700

    Added the current flatbuffers IPC code to the Spotlight Cookie Baker Process Project.

---
+++ Unity Projects/Spotlight Cookie Baker Process/Assets/ProcessorScript.cs
@@ -0,0 +1,12 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FCT.CookieBakerRT.SpotlightProcessing
+{
+	public class ProcessorScript 
+		: MonoBehaviour
+	{
+	}
+}
