f8f295b83701befde4e6c716bee61b72b8323b9f
Francisco Chavez-Tejeda <francisco-chavez@users.noreply.github.com>
2019-08-19 13:55:06 -0700

    I duplicated Prototype 02 as a starting point for Prototype 03. I removed a few items that weren't going to be needed. I added the IPC code that will be needed.

---
+++ Unity Projects/Prototype 03 Outside Process/Assets/Cookie Baker RT/Scripts/Bounds.cs
@@ -0,0 +1,21 @@
+﻿
+using UnityEngine;
+
+
+namespace FCT.CookieBakerP03
+{
+	public class Bounds
+	{
+
+		private Vector3 Extent;
+		private Vector3 Center;
+
+
+		public Bounds(UnityEngine.Bounds unityBounds)
+		{
+			Center = unityBounds.center;
+			Extent = unityBounds.extents;
+		}
+
+	}
+}
---
+++ Unity Projects/Prototype 03 Outside Process/Assets/Cookie Baker RT/Scripts/Vector3Ext.cs
@@ -0,0 +1,25 @@
+﻿
+using UnityEngine;
+
+
+namespace FCT.CookieBakerP03
+{
+	public static class Vector3Ext
+	{
+		/// <summary>
+		/// Returns the given Vector3 as a Vector4 with 1 for its 'w' value.
+		/// </summary>
+		public static Vector4 Position(this Vector3 pos)
+		{
+			return new Vector4(pos.x, pos.y, pos.z, 1.0f);
+		}
+
+		/// <summary>
+		/// Returns the given Vector3 as a Vector4 with a 0 for its 'w' value.
+		/// </summary>
+		public static Vector4 Direction(this Vector3 dir)
+		{
+			return new Vector4(dir.x, dir.y, dir.z, 0.0f);
+		}
+	}
+}
