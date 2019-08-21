e0565452a519b7e03304bcb8d20a5a70ddebe7aa
Francisco Chavez-Tejeda <francisco-chavez@users.noreply.github.com>
2019-08-20 18:38:55 -0700

    Started work on startup code for the Spotlight Cookie Baker Process.

--- Unity Projects/Spotlight Cookie Baker Process/Assets/ProcessorScript.cs
+++ Unity Projects/Spotlight Cookie Baker Process/Assets/ProcessorScript.cs
@@ -1,13 +1,100 @@
 ﻿
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Net;
+using System.Net.Sockets;
+
 using UnityEngine;
+
 
 namespace FCT.CookieBakerRT.SpotlightProcessing
 {
 	public class ProcessorScript 
 		: MonoBehaviour
 	{
-		public ComputeShader ComputeShader;
+
+		#region Attributes
+
+		public	ComputeShader		ComputeShader;
+
+		private int					_incommingMessagePortNumber;
+		private int					_outgoingMessagePortNumber;
+		private BackgroundWorker	_udpBackgoundMessenger;
+		private bool				_runUDPLoop					= false;
+
+		#endregion
+
+
+		private void Awake()
+		{
+			var commandLineArgs = System.Environment.GetCommandLineArgs();
+
+			var portInFound		= false;
+			var portOutFound	= false;
+			var argumentCount	= commandLineArgs.Length;
+
+
+			for (int i = 0; i < argumentCount - 1; i++)
+			{
+				if (commandLineArgs[i] == "-inputPort")
+				{
+					bool worked = int.TryParse(commandLineArgs[i + 1], out int portNum);
+					if (worked)
+					{
+						_incommingMessagePortNumber = portNum;
+						portInFound = true;
+					}
+				}
+				else if (commandLineArgs[i] == "-outputPort")
+				{
+					bool worked = int.TryParse(commandLineArgs[i + 1], out int portNum);
+					if (worked)
+					{
+						_outgoingMessagePortNumber = portNum;
+						portOutFound = true;
+					}
+				}
+			}
+
+			if (!portInFound && !portOutFound)
+				Application.Quit(-1);
+			if (!portInFound)
+				Application.Quit(-2);
+			if (!portOutFound)
+				Application.Quit(-3);
+
+			_runUDPLoop = true;
+		}
+
+		private void Start()
+		{
+			_udpBackgoundMessenger = new BackgroundWorker();
+			_udpBackgoundMessenger.DoWork += UDP_BackgroundThread;
+
+			_udpBackgoundMessenger.RunWorkerAsync();
+		}
+
+		private void UDP_BackgroundThread(object sender, DoWorkEventArgs e)
+		{
+			var endpointToListenTo	= new IPEndPoint(IPAddress.Any, _incommingMessagePortNumber);
+			var endpointOutgoing	= new IPEndPoint(IPAddress.Loopback, _outgoingMessagePortNumber);
+
+			// I know they have to call it something, and I'll admit that the options are limited, but did they 
+			// have to call it UdpClient when it can run as both a udp-client and a udp-server?
+			// -FCT
+			using (var udp = new UdpClient(endpointToListenTo))
+			{
+
+				var byteArray = CreateUpAndRunningMessage();
+
+				udp.Close();
+			}
+		}
+
+		private byte[] CreateUpAndRunningMessage()
+		{
+			throw new System.NotImplementedException();
+		}
 	}
 }
