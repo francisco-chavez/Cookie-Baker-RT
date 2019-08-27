58832fed799bb865b28719ec62074e64481bc8b5
Francisco Chavez-Tejeda <francisco-chavez@users.noreply.github.com>
2019-08-26 18:01:45 -0700

    Updating PowerShell script to compile new flatbuffers table.

--- Flatbuffers components/compile_flatbuffers_ipc_msg_csharp.ps1
+++ Flatbuffers components/compile_flatbuffers_ipc_msg_csharp.ps1
@@ -1,2 +1,2 @@
 ﻿
-flatc --csharp -o "./IPC MSG Serialization Code/CSharp/" "./schema/MyStructs.fbs" "./schema/CancelWorkloadMsg.fbs" "./schema/ProgressUpdateMsg.fbs" "./schema/UpAndRunningMsg.fbs" "./schema/WorkloadCompleteMsg.fbs" "./schema/WorkloadReceivedMsg.fbs" "./schema/WorkloadRequestMsg.fbs" "./schema/MessageRoot.fbs"
+flatc --csharp -o "./IPC MSG Serialization Code/CSharp/" "./schema/MyStructs.fbs" "./schema/CancelWorkloadMsg.fbs" "./schema/ProgressUpdateMsg.fbs" "./schema/ShutdownRequestMsg.fbs" "./schema/UpAndRunningMsg.fbs" "./schema/WorkloadCompleteMsg.fbs" "./schema/WorkloadReceivedMsg.fbs" "./schema/WorkloadRequestMsg.fbs" "./schema/MessageRoot.fbs"
