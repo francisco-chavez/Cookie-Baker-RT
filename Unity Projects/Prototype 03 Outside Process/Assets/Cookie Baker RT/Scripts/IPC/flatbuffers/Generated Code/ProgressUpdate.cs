d4ae3e20198f64cef630d4f89b4ab27ee6cf94b8
Francisco Chavez-Tejeda <francisco-chavez@users.noreply.github.com>
2019-08-26 18:06:40 -0700

    Updated Prototype 03 to latest version of IPC.

--- Unity Projects/Prototype 03 Outside Process/Assets/Cookie Baker RT/Scripts/IPC/flatbuffers/Generated Code/ProgressUpdate.cs
+++ Unity Projects/Prototype 03 Outside Process/Assets/Cookie Baker RT/Scripts/IPC/flatbuffers/Generated Code/ProgressUpdate.cs
@@ -20,19 +20,27 @@ public struct ProgressUpdate : IFlatbufferObject
 
   public int WorkloadID { get { int o = __p.__offset(4); return o != 0 ? __p.bb.GetInt(o + __p.bb_pos) : (int)0; } }
   public int CompletedSamples { get { int o = __p.__offset(6); return o != 0 ? __p.bb.GetInt(o + __p.bb_pos) : (int)0; } }
+  public int TotalSamples { get { int o = __p.__offset(8); return o != 0 ? __p.bb.GetInt(o + __p.bb_pos) : (int)0; } }
+  public bool CurrentlyRunning { get { int o = __p.__offset(10); return o != 0 ? 0!=__p.bb.Get(o + __p.bb_pos) : (bool)false; } }
 
   public static Offset<FCT.CookieBakerRT.IPC_DataFormat.ProgressUpdate> CreateProgressUpdate(FlatBufferBuilder builder,
       int WorkloadID = 0,
-      int CompletedSamples = 0) {
-    builder.StartTable(2);
+      int CompletedSamples = 0,
+      int TotalSamples = 0,
+      bool CurrentlyRunning = false) {
+    builder.StartTable(4);
+    ProgressUpdate.AddTotalSamples(builder, TotalSamples);
     ProgressUpdate.AddCompletedSamples(builder, CompletedSamples);
     ProgressUpdate.AddWorkloadID(builder, WorkloadID);
+    ProgressUpdate.AddCurrentlyRunning(builder, CurrentlyRunning);
     return ProgressUpdate.EndProgressUpdate(builder);
   }
 
-  public static void StartProgressUpdate(FlatBufferBuilder builder) { builder.StartTable(2); }
+  public static void StartProgressUpdate(FlatBufferBuilder builder) { builder.StartTable(4); }
   public static void AddWorkloadID(FlatBufferBuilder builder, int WorkloadID) { builder.AddInt(0, WorkloadID, 0); }
   public static void AddCompletedSamples(FlatBufferBuilder builder, int CompletedSamples) { builder.AddInt(1, CompletedSamples, 0); }
+  public static void AddTotalSamples(FlatBufferBuilder builder, int TotalSamples) { builder.AddInt(2, TotalSamples, 0); }
+  public static void AddCurrentlyRunning(FlatBufferBuilder builder, bool CurrentlyRunning) { builder.AddBool(3, CurrentlyRunning, false); }
   public static Offset<FCT.CookieBakerRT.IPC_DataFormat.ProgressUpdate> EndProgressUpdate(FlatBufferBuilder builder) {
     int o = builder.EndTable();
     return new Offset<FCT.CookieBakerRT.IPC_DataFormat.ProgressUpdate>(o);
